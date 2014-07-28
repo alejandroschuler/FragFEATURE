7b4d3d1c77f9f8ddc2161b50a6b4cb808786212c
Alejandro Schuler <alejandro.schuler@gmail.com>
2014-07-28 14:35:50 -0700

    finished micro clustering code

--- scripts/make_frag_binding_set.py
+++ scripts/make_frag_binding_set.py
@@ -6,35 +6,44 @@ import cPickle as pickle
 #print KB_HOME
 #print FragFEATURE_HOME
 
-querry_frag_num = 8027`
-micros_who_bind_querry = {}
+def main(querry_frag_string):
 
-for resatm in types:
-    print resatm
-    fragdata = load_resatm_KB_boundfrags(resatm)
-    microenvironment_list = []
-    for microenvironment_num, fraglist  in enumerate(fragdata):
-	if querry_frag_num in fraglist:
-            print microenvironment_num
-            microenvironment_list.append(microenvironment_num)
-    micros_who_bind_querry[resatm] = microenvironment_list
+    querry_frag_num = int(querry_frag_string)
+    micros_who_bind_querry = {}
+
+    for resatm in types:
+        print resatm
+        fragdata = load_resatm_KB_boundfrags(resatm)
+        microenvironment_list = []
+        for microenvironment_num, fraglist  in enumerate(fragdata):
+            if querry_frag_num in fraglist:
+                print microenvironment_num
+                microenvironment_list.append(microenvironment_num)
+        micros_who_bind_querry[resatm] = microenvironment_list
 
 ### STORE THE FRAGMENT-BINDING SET ### 
 
-outfile_pvar = 'micros_who_bind_' + str(querry_frag_num) + '.pvar'
-outfile_pvar = open(outfile_pvar, 'w')
-pickle.dump(micros_who_bind_querry, outfile_pvar)
-outfile_pvar.close()
+    outfile_pvar = 'micros_who_bind_' + str(querry_frag_num) + '.pvar'
+    outfile_pvar = open(outfile_pvar, 'w')
+    pickle.dump(micros_who_bind_querry, outfile_pvar)
+    outfile_pvar.close()
 
 ### WRITE TO CSV FILE ###
 
-outfile_txt = 'micros_who_bind_' + str(querry_frag_num) + '.txt'
-outfile_txt = open(outfile_txt, 'w')
-outfile_txt.write(str(micros_who_bind_querry))
-outfile_txt.close()
+    outfile_txt = 'micros_who_bind_' + str(querry_frag_num) + '.txt'
+    outfile_txt = open(outfile_txt, 'w')
+    outfile_txt.write(str(micros_who_bind_querry))
+    outfile_txt.close()
 
 #note that in these files, a reference to microenv #0 means line #1, etc. Everything starts from 0.
 
 ### this code has been verified manually by looking at lines 0:9 of PHE.PSEU.ALL.boundfrags
 
-print 'DING!'
+    print 'DING!'
+    return
+
+#################################################################################
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1])
+
--- scripts/make_micro_homo_clusters.py
+++ scripts/make_micro_homo_clusters.py
@@ -2,10 +2,17 @@ import cPickle as pickle
 from load_directorypaths import *
 from load_datafiles import load_resatm_KB_ann, load_seqclusterid
 from microenvironment_types import types
+import cPickle as pickle
 
 def main():
     prot_clusters = load_seqclusterid('95') 
+    print 'File loaded'
     micro_clusters = cluster_micros(prot_clusters)
+    outfile = 'micro_clusters.pvar'
+    outfile = open(outfile,'w')
+    pickle.dump(micro_clusters, outfile)
+
+    return
 
 ################################################################################
 def invert_dict(dict_by_prot):    
@@ -26,16 +33,26 @@ def cluster_micros(dict_by_tag):
     for resatm in types:
         type_clusters = {}
         anndata = load_resatm_KB_ann(resatm)
+        print "loaded annotations for %s" % (resatm)
         for micro_num, micro_ann in enumerate(anndata):
             # look up the protein the micro belongs to with micro.tag
             # then look up the cluster that protein belongs to
-            clusterID = dict_by_tag[micro.tag] 
-            if clusterID not in type_clusters:
-                #assign the micro line number to the cluster
-                type_clusters[clusterID] = [micro_num]
-            else:
-                type_clusters[clusterID].append(micro_num)
+            try:
+                clusterID = dict_by_tag[micro_ann.tag] 
+                #print "%s %s with tag %s belongs to cluster %s" % (resatm, str(micro_num), micro_ann.tag, str(clusterID))
+                if clusterID not in type_clusters:
+                    #assign the micro line number to the cluster
+                    type_clusters[clusterID] = [micro_num]
+                else:
+                    type_clusters[clusterID].append(micro_num)
+            except KeyError:
+                print "%s %s with tag %s has no cluster" % (resatm, str(micro_num), micro_ann.tag)
         # put the dictionary for this micro type into the master dictionary
         micro_clusters[resatm] = type_clusters
 
     return micro_clusters
+
+
+################################################################################
+if __name__ == "__main__":
+    main()
